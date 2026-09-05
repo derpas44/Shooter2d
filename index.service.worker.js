73b3282a3c3e278210537d1b3401ac62204decd2
DerPas44 <pascal.hwellendorf2@gmail.com>
2026-09-05 21:19:23 +0200

    324
    679011

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1788551487|4450592865';
+const CACHE_VERSION = '1788635888|188350460';
 /** @type {string} */
 const CACHE_PREFIX = 'Splitscreen Game-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
